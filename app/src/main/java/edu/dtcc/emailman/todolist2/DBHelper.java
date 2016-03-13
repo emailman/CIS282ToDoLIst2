8ea1c468a3f9baa5d766d834fd976c5a571b33e9
emailman <emailman@dtcc.edu>
2016-03-13 19:49:13 -0400

    coding in progress

---
+++ app/src/main/java/edu/dtcc/emailman/todolist2/DBHelper.java
@@ -0,0 +1,104 @@
+package edu.dtcc.emailman.todolist2;
+
+/*
+ * Created by emailman on 3/13/16.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper{
+
+    //Define the database and tables
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "toDo_DB";
+    private static final String DATABASE_TABLE = "toDo_Items";
+
+    // Define the column names for the table
+    private static final String TASK_ID = "_id";
+    private static final String DESCRIPTION = "description";
+    private static final String IS_DONE = "is_done";
+
+    private int taskCount;
+
+    // Constructor
+    public DBHelper (Context context) {
+        super (context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String table = "CREATE TABLE" + DATABASE_TABLE + "(" +
+                TASK_ID + " INTEGER KEY AUTOINCREMENT, " +
+                DESCRIPTION + " TEXT, " +
+                IS_DONE + " INTEGER" +")";
+        sqLiteDatabase.execSQL(table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+        // Drop old table, if it exists
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS" + DATABASE_TABLE);
+
+        // Create table again
+        onCreate(sqLiteDatabase);
+    }
+
+    // Add a new task
+    public void addToDoItem(ToDo_Item task) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        // Set the new values
+        values.put(DESCRIPTION, task.getDescription());
+        values.put(IS_DONE, task.getIs_done());
+
+        // Insert the row in the table
+        db.insert(DATABASE_TABLE, null, values);
+        taskCount++;
+
+        // Close the connection
+        db.close();
+    }
+
+    public List<ToDo_Item> getAllTasks() {
+        List<ToDo_Item> todoList = new ArrayList<ToDo_Item>();
+
+        String selectQuery = "SELECT * FROM " + DATABASE_TABLE;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+    return todoList;
+    }
+
+    // On click handler to clear the list and the table
+    public void clearAll (List<ToDo_Item> list) {
+        list.clear();
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(DATABASE_TABLE, null, new String[] {});
+        db.close();
+    }
+
+    // On click hadler to update
+    public void update_Task(ToDo_Item task) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(DESCRIPTION, task.getDescription());
+        values.put(IS_DONE, task.getIs_done());
+
+        db.update(DATABASE_TABLE, values, TASK_ID + " = ?",
+                new String[]{String.valueOf(task.get_id())});
+
+        db.close();
+    }
+}
---
+++ app/src/main/java/edu/dtcc/emailman/todolist2/ToDo_Item.java
@@ -0,0 +1,49 @@
+package edu.dtcc.emailman.todolist2;
+
+/*
+ * Created by emailman on 3/13/16.
+ */
+
+class ToDo_Item {
+
+    private int _id;
+    private String description;
+    private int is_done;
+
+    public ToDo_Item() {
+    }
+
+    public ToDo_Item(int _id) {
+        this._id = _id;
+    }
+
+    public ToDo_Item(int id, String desc, int done) {
+        _id = id;
+        description = desc;
+        is_done = done;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getIs_done() {
+        return is_done;
+    }
+
+    public void setIs_done(int is_done) {
+        this.is_done = is_done;
+    }
+}
